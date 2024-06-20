9753eafb4189a6b8861298790d8d97981753778d
Ariel <arielmontoya200609@gmail.com>
2024-06-20 15:14:16 -0300

    subir archivo

---
+++ TP5/Parte2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+echo "Ingrese un nombre para verificar el género probable:"
+read nombre
+
+URL="https://api.genderize.io/?name=$nombre"
+
+
+respuesta=$(curl -s "$URL")
+
+
+if [[ $? -ne 0 ]]; then
+  echo "Error al conectarse a la API. Por favor, verifica tu conexión a internet."
+  exit 1
+fi
+
+
+nombre_respuesta=$(echo "$respuesta" | jq -r '.name')
+genero=$(echo "$respuesta" | jq -r '.gender')
+
+if [[ "$genero" == "null" ]]; then
+  echo "No se pudo determinar el género para el nombre '$nombre'."
+else
+  echo "Para el nombre '$nombre', el género probable es '$genero'."
+fi
