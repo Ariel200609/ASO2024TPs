150b567d4ecf75b5fbecb94c7d1e4a21e04f0057
Ariel <arielmontoya200609@gmail.com>
2024-06-21 18:17:57 -0300

    cambios realizados

--- TP5/Parte2.sh
+++ TP5/Parte2.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-
 echo "Ingrese un nombre para verificar el género probable:"
 read nombre
 
 URL="https://api.genderize.io/?name=$nombre"
 
-
 respuesta=$(curl -s "$URL")
-
 
 if [[ $? -ne 0 ]]; then
   echo "Error al conectarse a la API. Por favor, verifica tu conexión a internet."
   exit 1
 fi
-
 
 nombre_respuesta=$(echo "$respuesta" | jq -r '.name')
 genero=$(echo "$respuesta" | jq -r '.gender')
@@ -22,5 +18,8 @@ genero=$(echo "$respuesta" | jq -r '.gender')
 if [[ "$genero" == "null" ]]; then
   echo "No se pudo determinar el género para el nombre '$nombre'."
 else
+  if [[ "$genero" == "male" ]]; then
+    genero="masculino"
+  fi  
   echo "Para el nombre '$nombre', el género probable es '$genero'."
 fi
